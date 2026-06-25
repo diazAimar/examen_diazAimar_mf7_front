d414569dba28f8dd2aa6f778094196b300a84964
diazAimar <diazaimarf@gmail.com>
2026-06-25 00:44:52 -0300

    feat: pasar a antd, inicializar react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ConfigProvider } from "antd";
+import esES from "antd/locale/es_ES";
 import "./index.css";
 import AppRouter from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRouter />
+    <ConfigProvider locale={esES}>
+      <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
+    </ConfigProvider>
   </StrictMode>,
 );
