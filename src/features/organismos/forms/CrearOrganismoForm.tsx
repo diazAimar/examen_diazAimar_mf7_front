530bcc17604b206a4129455b68f06676aa2ff2a7
diazAimar <diazaimarf@gmail.com>
2026-06-25 14:05:55 -0300

    feat: usar select para ciudad y fuero

--- src/features/organismos/forms/CrearOrganismoForm.tsx
+++ src/features/organismos/forms/CrearOrganismoForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, Select } from "antd";
 import { useForm, Controller } from "react-hook-form";
 import useCreateOrganismo from "../../../hooks/organismos/useCreateOrganismo";
 import { handleValidationErrors } from "../../../utils/handleValidationErrors";
@@ -34,76 +34,66 @@ const CrearOrganismoForm = ({ onSuccess }: CrearOrganismoFormProps) => {
         onSuccess();
       },
       onError: (error) => {
-        handleValidationErrors(
-          error as AxiosError<ApiResponse<null>>,
-          setError,
-        );
+        handleValidationErrors(error as AxiosError<ApiResponse<null>>, setError);
       },
     });
   };
 
   return (
-    <Form
-      layout="vertical"
-      className="flex flex-col"
-      onFinish={() => handleSubmit(onSubmit)()}
-    >
-      <Form.Item
-        label="Nombre"
-        validateStatus={errors.nombre ? "error" : ""}
-        help={errors.nombre?.message}
-      >
+    <Form layout="vertical" className="flex flex-col" onFinish={() => handleSubmit(onSubmit)()}>
+      <Form.Item label="Nombre" validateStatus={errors.nombre ? "error" : ""} help={errors.nombre?.message}>
         <Controller
           name="nombre"
           control={control}
           rules={{ required: "El nombre es requerido" }}
-          render={({ field }) => (
-            <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />
-          )}
+          render={({ field }) => <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Carátula"
-        validateStatus={errors.caratula ? "error" : ""}
-        help={errors.caratula?.message}
-      >
+      <Form.Item label="Carátula" validateStatus={errors.caratula ? "error" : ""} help={errors.caratula?.message}>
         <Controller
           name="caratula"
           control={control}
           rules={{ required: "La carátula es requerida" }}
-          render={({ field }) => (
-            <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />
-          )}
+          render={({ field }) => <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Ciudad"
-        validateStatus={errors.ciudad ? "error" : ""}
-        help={errors.ciudad?.message}
-      >
+      <Form.Item label="Ciudad" validateStatus={errors.ciudad ? "error" : ""} help={errors.ciudad?.message}>
         <Controller
           name="ciudad"
           control={control}
           rules={{ required: "La ciudad es requerida" }}
           render={({ field }) => (
-            <Input {...field} placeholder="Ej: Neuquén" />
+            <Select
+              {...field}
+              onChange={field.onChange}
+              options={[
+                { value: "Neuquén", label: "Neuquén" },
+                { value: "Zapala", label: "Zapala" },
+                { value: "Junín de los Andes", label: "Junín de los Andes" },
+              ]}
+            />
           )}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Fuero"
-        validateStatus={errors.fuero ? "error" : ""}
-        help={errors.fuero?.message}
-      >
+      <Form.Item label="Fuero" validateStatus={errors.fuero ? "error" : ""} help={errors.fuero?.message}>
         <Controller
           name="fuero"
           control={control}
           rules={{ required: "El fuero es requerido" }}
           render={({ field }) => (
-            <Input {...field} placeholder="Ej: Civil" />
+            <Select
+              {...field}
+              onChange={field.onChange}
+              options={[
+                { value: "Ejecutivos", label: "Ejecutivos" },
+                { value: "Civil", label: "Civil" },
+                { value: "Laboral", label: "Laboral" },
+                { value: "Familia", label: "Familia" },
+              ]}
+            />
           )}
         />
       </Form.Item>
--- src/features/organismos/forms/EditarOrganismoForm.tsx
+++ src/features/organismos/forms/EditarOrganismoForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form } from "antd";
+import { Button, Input, Form, Select } from "antd";
 import { useForm, Controller } from "react-hook-form";
 import { handleValidationErrors } from "../../../utils/handleValidationErrors";
 import type { AxiosError } from "axios";
@@ -17,10 +17,7 @@ interface EditarOrganismoFormValues {
   fuero: string;
 }
 
-const EditarOrganismoForm = ({
-  onSuccess,
-  organismo,
-}: EditarOrganismoFormProps) => {
+const EditarOrganismoForm = ({ onSuccess, organismo }: EditarOrganismoFormProps) => {
   const {
     control,
     handleSubmit,
@@ -47,77 +44,68 @@ const EditarOrganismoForm = ({
           onSuccess();
         },
         onError: (error) => {
-          handleValidationErrors(
-            error as AxiosError<ApiResponse<null>>,
-            setError,
-          );
+          handleValidationErrors(error as AxiosError<ApiResponse<null>>, setError);
         },
-      },
+      }
     );
   };
 
   return (
-    <Form
-      layout="vertical"
-      className="flex flex-col"
-      onFinish={() => handleSubmit(onSubmit)()}
-    >
-      <Form.Item
-        label="Nombre"
-        validateStatus={errors.nombre ? "error" : ""}
-        help={errors.nombre?.message}
-      >
+    <Form layout="vertical" className="flex flex-col" onFinish={() => handleSubmit(onSubmit)()}>
+      <Form.Item label="Nombre" validateStatus={errors.nombre ? "error" : ""} help={errors.nombre?.message}>
         <Controller
           name="nombre"
           control={control}
           rules={{ required: "El nombre es requerido" }}
-          render={({ field }) => (
-            <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />
-          )}
+          render={({ field }) => <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Carátula"
-        validateStatus={errors.caratula ? "error" : ""}
-        help={errors.caratula?.message}
-      >
+      <Form.Item label="Carátula" validateStatus={errors.caratula ? "error" : ""} help={errors.caratula?.message}>
         <Controller
           name="caratula"
           control={control}
           rules={{ required: "La carátula es requerida" }}
-          render={({ field }) => (
-            <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />
-          )}
+          render={({ field }) => <Input {...field} placeholder="Ej: Juzgado Civil Neuquén" />}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Ciudad"
-        validateStatus={errors.ciudad ? "error" : ""}
-        help={errors.ciudad?.message}
-      >
+      <Form.Item label="Ciudad" validateStatus={errors.ciudad ? "error" : ""} help={errors.ciudad?.message}>
         <Controller
           name="ciudad"
           control={control}
           rules={{ required: "La ciudad es requerida" }}
           render={({ field }) => (
-            <Input {...field} placeholder="Ej: Neuquén" />
+            <Select
+              {...field}
+              defaultValue={organismo.ciudad}
+              onChange={field.onChange}
+              options={[
+                { value: "Neuquén", label: "Neuquén" },
+                { value: "Zapala", label: "Zapala" },
+                { value: "Junín de los Andes", label: "Junín de los Andes" },
+              ]}
+            />
           )}
         />
       </Form.Item>
 
-      <Form.Item
-        label="Fuero"
-        validateStatus={errors.fuero ? "error" : ""}
-        help={errors.fuero?.message}
-      >
+      <Form.Item label="Fuero" validateStatus={errors.fuero ? "error" : ""} help={errors.fuero?.message}>
         <Controller
           name="fuero"
           control={control}
           rules={{ required: "El fuero es requerido" }}
           render={({ field }) => (
-            <Input {...field} placeholder="Ej: Civil" />
+            <Select
+              {...field}
+              defaultValue={organismo.fuero}
+              onChange={field.onChange}
+              options={[
+                { value: "Neuquén", label: "Neuquén" },
+                { value: "Zapala", label: "Zapala" },
+                { value: "Junín de los Andes", label: "Junín de los Andes" },
+              ]}
+            />
           )}
         />
       </Form.Item>
